c7407a08b910da917457577fda4dbee3969e4fb8
luzhaigo <gitluzhai@gmail.com>
2024-05-02 00:15:43 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { crx } from '@crxjs/vite-plugin';
+import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [react(), tsconfigPaths()],
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    plugins: [react(), tsconfigPaths(), crx({ manifest })],
   };
 });
